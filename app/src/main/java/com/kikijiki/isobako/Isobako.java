08c144a38377203bd533150d205c8d9c2df6cd2a
KIKIJIKI <dev@kikijiki.com>
2014-10-10 08:51:08 +0900

    Updated license

--- app/src/main/java/com/kikijiki/isobako/Isobako.java
+++ app/src/main/java/com/kikijiki/isobako/Isobako.java
@@ -1,18 +1,18 @@
 /*
-Copyright 2011 Matteo Bernacchia <android@kikijiki.com>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2014 Matteo Bernacchia <kikijikispaccaspecchi@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.kikijiki.isobako;
 
@@ -285,7 +285,9 @@ public class Isobako extends WallpaperService {
         }
 
         private void draw() {
-            if(!this.visible){ return; }
+            if (!this.visible) {
+                return;
+            }
             SurfaceHolder holder = getSurfaceHolder();
             Canvas c;
 
